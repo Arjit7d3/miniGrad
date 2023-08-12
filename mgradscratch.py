c24606f3d0c84314c55644c2c6f21a41fcb7a86a
Arjit7d3 <129239596+Arjit7d3@users.noreply.github.com>
2023-08-12 15:42:23 +0530

    Update mgradscratch.py
    removed the test code

--- mgradscratch.py
+++ mgradscratch.py
@@ -90,31 +90,3 @@ class Value:
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
-
-#x1 = Value(2.0)
-#x2 = Value(0.0)
-#w1 = Value(-3.0)
-#w2 = Value(1.0)
-#b = Value(6.8813735870195432)
-#x1w1 = x1 * w1
-#x2w2 = x2 * w2
-#x1w1_x2w2 = x1w1 + x2w2
-#n = x1w1_x2w2 + b
-#o = n.tanh()
-#o.backward()
-
-x1 = Value(2.0)
-x2 = Value(0.0)
-w1 = Value(-3.0)
-w2 = Value(1.0)
-b = Value(6.8813735870195432)
-x1w1 = x1 * w1
-x2w2 = x2 * w2
-x1w1_x2w2 = x1w1 + x2w2
-n = x1w1_x2w2 + b
-c = (2*n).exp()
-o = (c - 1) / (c + 1)
-print(o)
-o.backward()
-
-print(x1.grad)
